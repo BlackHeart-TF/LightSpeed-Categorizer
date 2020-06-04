f58f350a6731dbc09db8f492e5fe9d7500d49fe4
PetNoire <noire@gmx.ca>
2020-06-04 09:45:25 -0600

    seems to be okay, starting on interface

---
+++ Categorizer/CatalogProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Categorizer
+{
+    interface CatalogProvider
+    {
+        void MoveCategory(object from, object to);
+        void CreateCategory(string name, string path);
+        void DeleteCategory(string path);
+        object GetCategory(string name);
+        object[] GetAllCategories();
+    }
+}
---
+++ Categorizer/LSImplementation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Categorizer
+{
+    class LSImplementation : CatalogProvider
+    {
+        public void MoveCategory(object from, object to)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateCategory(string name, string path)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteCategory(string path)
+        {
+            throw new NotImplementedException();
+        }
+        public object GetCategory(string name)
+        {
+            throw new NotImplementedException();
+        }
+        public object[] GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
